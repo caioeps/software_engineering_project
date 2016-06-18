437fa28be3bc2e41506341eb10cc65bbcbc738d9
caioeps <caioergos@gmail.com>
2016-06-18 19:41:27 -0300

    remove template js and css

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+map = new GMaps({
+  div: '#map',
+  lat: -3.7572221,
+  lng: -38.5356611,
+  zoom: 13
+});
