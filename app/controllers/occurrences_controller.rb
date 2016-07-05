d26e1b272f7ab196520088f2dc38911bc7115bb4
caioeps <caioergos@gmail.com>
2016-07-05 14:56:44 -0300

    controllers

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -1,33 +1,75 @@
 class OccurrencesController < ApplicationController
+  before_action :set_occurrence, only: [:show, :edit, :update, :destroy]
+
+  # GET /occurrences
+  # GET /occurrences.json
   def index
     @occurrences = Occurrence.all
   end
 
+  # GET /occurrences/1
+  # GET /occurrences/1.json
+  def show
+  end
+
+  # GET /occurrences/new
   def new
     @occurrence = Occurrence.new
   end
 
-  def create
-    @occurrence = Occurrence.new(occurrence_params)
+  # GET /occurrences/1/edit
+  def edit
+  end
 
-    if @occurrence.save
-      respond_to do |format|
-        format.html { redirect_to :back, notice: 'Ocorrência salva' }
-        format.json { render json: @occurrence }
-        format.js
-      end
-    else
-      respond_to do |format|
-        format.html { redirect_to :back, notice: 'Ocorrência não salva' }
-        format.json { render json: @occurrence }
-        format.js
+  # POST /occurrences
+  # POST /occurrences.json
+  def create
+    @occurrence      = Occurrence.new(occurrence_params)
+    @occurrence.user = current_user
+
+    respond_to do |format|
+      if @occurrence.save
+        format.html { redirect_to root_path, notice: 'Occurrence was successfully created.' }
+        format.json { render :show, status: :created, location: @occurrence }
+      else
+        format.html { render :new }
+        format.json { render json: @occurrence.errors, status: :unprocessable_entity }
       end
     end
   end
 
-private
-
-  def occurrence_params
-    params.require(:occurrence).permit(:location, :hour, :description)
+  # PATCH/PUT /occurrences/1
+  # PATCH/PUT /occurrences/1.json
+  def update
+    respond_to do |format|
+      if @occurrence.update(occurrence_params)
+        format.html { redirect_to @occurrence, notice: 'Occurrence was successfully updated.' }
+        format.json { render :show, status: :ok, location: @occurrence }
+      else
+        format.html { render :edit }
+        format.json { render json: @occurrence.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  # DELETE /occurrences/1
+  # DELETE /occurrences/1.json
+  def destroy
+    @occurrence.destroy
+    respond_to do |format|
+      format.html { redirect_to occurrences_url, notice: 'Occurrence was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_occurrence
+      @occurrence = Occurrence.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def occurrence_params
+      params.require(:occurrence).permit(:description, :date, :hour, :rating, :latitude, :longitude, :location)
+    end
 end
