d9c7fe2f16231e721c69467a544b40ddee296fcd
caioeps <caioergos@gmail.com>
2016-07-05 14:51:56 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :occurrences
   devise_for :users
 
   root 'map#map'
 
-  resources :occurrences
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
