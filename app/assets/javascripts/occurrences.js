81404eb2b84d5506ad77bcff2f3eca511e298438
caioeps <caioergos@gmail.com>
2016-07-05 15:01:57 -0300

    use JS and jQuery notation

--- app/assets/javascripts/occurrences.js
+++ app/assets/javascripts/occurrences.js
@@ -1,49 +1,49 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-hour_field = $('#occurrence_hour');
-date_field = $('#occurrence_date')
-hour_error_div = $('#occurrence_hour_error');
-date_error_div = $('#occurrence_date_error');
-submit_button = $('#submit_button')
+$hourField = $('#occurrence_hour');
+$dateField = $('#occurrence_date')
+$hourErrorDiv = $('#occurrence_hour_error');
+$dateErrorDiv = $('#occurrence_date_error');
+$submitButton = $('#$submit_button')
 
 $(document).ready(function(){
-  hour_error_div.hide();
-  date_error_div.hide();
+  $hourErrorDiv.hide();
+  $dateErrorDiv.hide();
 });
 
 function validateHour() {
-  hour = parseInt(hour_field.val().split(':')[0]);
-  minutes = parseInt(hour_field.val().split(':')[1]);
+  hour = parseInt($hourField.val().split(':')[0]);
+  minutes = parseInt($hourField.val().split(':')[1]);
 
-  if (hasAnyText(hour_field) && (hour < 0 || hour > 24 || minutes < 0 || minutes > 60)){
-    hour_error_div.show();
+  if (hasAnyText($hourField) && (hour < 0 || hour > 24 || minutes < 0 || minutes > 60)){
+    $hourErrorDiv.show();
     disable_submit_button(true)
-    submit_button.removeClass('red');
-    submit_button.addClass('grey');
+    $submitButton.removeClass('red');
+    $submitButton.addClass('grey');
   } else {
-    hour_error_div.hide();
+    $hourErrorDiv.hide();
     disable_submit_button(false)
-    submit_button.removeClass('grey');
-    submit_button.addClass('red');
+    $submitButton.removeClass('grey');
+    $submitButton.addClass('red');
   }
 }
 
 function validateDate() {
-  day = parseInt(date_field.val().split(':')[0]);
-  month = parseInt(date_field.val().split(':')[1]);
-  year = parseInt(date_field.val().split(':')[2]);
+  day = parseInt($dateField.val().split(':')[0]);
+  month = parseInt($dateField.val().split(':')[1]);
+  year = parseInt($dateField.val().split(':')[2]);
 
-  if (hasAnyText(date_field) && (day < 0 || day > 31 || month < 0 || month > 12 || year < 0)){
-    date_error_div.show();
+  if (hasAnyText($dateField) && (day < 0 || day > 31 || month < 0 || month > 12 || year < 0)){
+    $dateErrorDiv.show();
     disable_submit_button(true)
-    submit_button.removeClass('red');
-    submit_button.addClass('grey');
+    $submitButton.removeClass('red');
+    $submitButton.addClass('grey');
   } else {
-    date_error_div.hide();
+    $dateErrorDiv.hide();
     disable_submit_button(false)
-    submit_button.removeClass('grey');
-    submit_button.addClass('red');
+    $submitButton.removeClass('grey');
+    $submitButton.addClass('red');
   }
 }
 
@@ -52,5 +52,5 @@ function hasAnyText(div) {
 }
 
 function disable_submit_button(option){
-  submit_button.prop("disabled",option);
+  $submitButton.prop("disabled",option);
 }
